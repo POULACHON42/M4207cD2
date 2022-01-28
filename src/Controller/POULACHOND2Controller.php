55f51e20e37c103a3933c90bc3aad849324a04f0
POULACHON42 <hugo.poulachon@etu.univ-st-etienne.fr>
2022-01-28 11:56:28 +0100

    TP2 avec le formulaire avec login et mdp

--- src/Controller/POULACHOND2Controller.php
+++ src/Controller/POULACHOND2Controller.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class POULACHOND2Controller extends AbstractController
 {
@@ -17,4 +18,35 @@ class POULACHOND2Controller extends AbstractController
             'controller_name' => 'POULACHOND2Controller',
         ]);
     }
+
+	/**
+	* @Route("/", name="home")
+	*/
+	
+    	public function home()
+	{
+		return $this->render('poulachond2/home.html.twig', [
+		]);
+}
+    
+    /**
+	* @Route("/traitement", name="traitement")
+	*/
+    public function traitement(Request $request) : Response
+	{
+        $nom = $request->request->get("login");
+		return $this->render('poulachond2/traitement.html.twig', [
+            'titre' => 'confirmation',
+            'login' => $nom,
+		]);
+    }
+
+
+
+
+
+
+
+
+
 }
