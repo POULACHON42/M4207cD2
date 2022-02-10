99cc8a66893e74e70cafd0d2ce63644a6cc6298d
POULACHON42 <hugo.poulachon@etu.univ-st-etienne.fr>
2022-02-10 15:52:36 +0100

    Numéro ID et déconnexion

--- src/Controller/POULACHOND2Controller.php
+++ src/Controller/POULACHOND2Controller.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Utilisateur;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
 class POULACHOND2Controller extends AbstractController
@@ -32,32 +33,56 @@ class POULACHOND2Controller extends AbstractController
 		]);
     }
     
+
     /**
 	* @Route("/traitement", name="traitement")
 	*/
 
-    public function traitement(Request $request, EntityManagerInterface $manager) : Response
+    public function traitement(Request $request, EntityManagerInterface $manager, SessionInterface $session) : Response
 	{
+        $profil = $session -> get('chiffre');
         $username = $request -> request -> get("username");
         $password = $request -> request -> get("password");
         $repo = $manager -> getRepository (Utilisateur::class);
         $username2 =  $repo -> findOneby (["login" => $username]);
+                
         
-        if($username2==NULL)
-          $msg="Utilisateur inconnu";
-        elseif ($password == $username2 -> getPassword())
+        if($username2==NULL){
+            $msg="Utilisateur inconnu";
+            $profil = 0;
+        }
+        elseif ($password == $username2 -> getPassword()){
             $msg="Vos identifiants sont corrects !";
-        else
+            $profil = $username2->getId();
+            $session -> set('chiffre',$profil);
+            $profil = $session -> get('chiffre');
+            }
+        else{
             $msg = "Il ne s'agit pas du bon identifiant ou  mot de passe";
+            $profil = 0;
+        }
 
         return $this->render('poulachond2/verification.html.twig', [
             'titre' => 'confirmation',
             'login' => $username,
             'pass' => $password,
             'ms' => $msg,
+            'id' => $profil
         ]);   
+        
     }
 
+    /**
+	* @Route("/deco", name="deco")
+	*/
+    public function deconnexion(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
+    {
+        $session -> set('numero', 0);
+        $session -> clear();
+
+    return $this->render('poulachond2/login.html.twig');
+    
+    }
     /**
 	* @Route("/creaUser", name="creaUser")
 	*/
@@ -88,12 +113,12 @@ public function createUser(Request $request, EntityManagerInterface $manager) : 
     /**
 	* @Route("/liste_user", name="liste_user")
 	*/
-    public function listeUser(Request $request, EntityManagerInterface $manager) : Response
+    public function listeUser(Request $request, EntityManagerInterface $manager, SessionInterface $session) : Response
     {
         $listeUser = $manager -> getRepository(Utilisateur::class)-> findAll();
-
+        $profil = $session -> get('chiffre');
         return $this->render('poulachond2/users.html.twig', [
-            'liste_user' => $listeUser
+            'liste_user' => $listeUser,'id' => $profil
         ]);
 
     }
